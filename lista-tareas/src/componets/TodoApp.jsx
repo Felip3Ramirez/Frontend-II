67256dc8644250dfe37864ab58c6feb37ccd328f
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-02-19 18:31:44 -0500

    update de practica

--- lista-tareas/src/componets/TodoApp.jsx
+++ lista-tareas/src/componets/TodoApp.jsx
@@ -1,29 +1,57 @@
 import { useState } from "react";
-export default 
+import Todo from "./Todo";
 
+export default function TodoApp() {
+  const [title, setTitle] = useState("Hola");
+  const [todos, setTodos] = useState([]);
 
-function TodoApp() {
-    const [title, setTitle] = useState("Hola");
-function handleClick(e){
-    e.preventDefault();
-    setTitle("Yere");
-}
-function handleChange (e){
+  function handleChange(e) {
     const value = e.target.value;
     setTitle(value);
-}
-    return (
-        <div className="todoContainer">
-            <form className="todoCreateForm" >
-                <input onChange={handleChange} type="text" className="todoInput" value={title} />
-                <input
-                    onClick={handleClick}
-                    type="submit"
-                    value="Create Todo"
-                    className="buttonCreate"
-                />
-                {title}
-            </form>
-        </div>
-    );
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false,
+    };
+    const temp = [...todos];
+    temp.unshift(newTodo);
+
+    setTodos(temp);
+
+  }
+  function handleUpdate(id,value){
+    const temp = [...todos];
+    const item = temp.find(item => item.id === id);
+    item.title= value;
+    setTodos(temp);
+  }
+  return (
+    <div className="todoContainer">
+      <form className="todoCreateForm" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          className="todoInput"
+          value={title}
+        />
+        <input
+          onClick={handleSubmit}
+          type="submit"
+          value="Create Todo"
+          className="buttonCreate"
+        />
+      </form>
+
+      <div className="todosContainer">
+        {todos.map((item) => (
+          <Todo key={item.id} item={item} onUpdate={handleUpdate}/>
+        ))}
+      </div>
+      
+    </div>
+  );
 }
