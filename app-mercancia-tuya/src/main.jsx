b0ae36dbe0501748dd80ffc6b128e4a3d2571889
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-03-12 15:51:02 -0500

    Update main.jsx

--- app-mercancia-tuya/src/main.jsx
+++ app-mercancia-tuya/src/main.jsx
@@ -2,14 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PanelControl from './pages/PanelControl'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Login from './pages/auth/Login'
-
+let router = createBrowserRouter([
+  {
+    path:"/login",
+    element:<Login></Login>
+  },
+  {
+    path:"/panel",
+    element:<PanelControl></PanelControl>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PanelControl></PanelControl>
+    <RouterProvider></RouterProvider>
+    {/* <PanelControl></PanelControl> */}
     {/* <PanelClientes></PanelClientes> */}
     {/* <PanelEnvios></PanelEnvios> */}
-    <Login></Login>
+    
   </StrictMode>,
 )
